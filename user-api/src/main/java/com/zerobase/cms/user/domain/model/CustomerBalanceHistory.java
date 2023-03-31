f451be509aeab22db459685056b82d98a8f0210e
hennessy_on_melody <attempt3699@gmail.com>
2023-03-31 16:46:41 +0900

    feat : [006-CUSTOMER-BALANCE] Customer 에 대한 잔액 조회 구현

---
+++ user-api/src/main/java/com/zerobase/cms/user/domain/model/CustomerBalanceHistory.java
@@ -0,0 +1,30 @@
+package com.zerobase.cms.user.domain.model;
+
+
+import lombok.*;
+import org.hibernate.envers.AuditOverride;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@AuditOverride(forClass = BaseEntity.class)
+public class CustomerBalanceHistory extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(targetEntity = Customer.class, fetch = FetchType.LAZY)
+    private Customer customer;
+    private Integer changeMoney;
+    private Integer currentMoney;
+    private String fromMessage;
+    private String description;
+
+
+}
